57b33d47b33c10e15a9eb43c204231e455733ba0
Tyler Ruff <hello@blazed.space>
2022-08-16 23:57:49 -0400

    BLZ

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # blz-cli
 Command line tool for generating Blazed Fire/Ice projects, interpreting BLZ-SH script, and more.
+
+## How to Install
+```sh
+npm install -g blz-cli
+```
+
+### How to Use
+Run any of the following commands after installing to generate a new fire or ice project:
+```sh
+new-fire
+new-fire-angular
+new-fire-react
+new-fire-svelte
+new-ice-php
+new-ice-node
+new-ice-ruby
+new-ice-java
+new-ice-dotnet
+```
---
+++ bin/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+console.log("**** Welcome to blz-cli, created by Blazed Labs LLC. <blazedlabs.com> ****");
+console.log("Commands:");
+console.log("`new fire` - Spins up a new Fire for HTML5 project.");
+console.log("`new fire-angular` - Spins up a new Fire for Angular project.");
+console.log("`new fire-react` - Spins up a new Fire for React project.");
+console.log("`new fire-svelte` - Spins up a new Fire for Svelte project.");
+console.log("`new ice-php` - Spins up a new Ice for PHP project.");
+console.log("`new ice-ruby` - Spins up a new Ice for Ruby on Rails project.");
+console.log("`new ice-node` - Spins up a new Ice for Javascript (Node.js) project.");
+console.log("`new ice-java` - Spins up a new Ice for Java (Spring Boot) project.");
+console.log("`new ice-dotnet` - Spins up a new Ice for C#/.NET (Blazor) project.");
+console.log("Published by: Blazed Publishing <blazed.xyz>.");
+console.log("Created by: Tyler Ruff <tyler-ruff.com>.");
+console.log("Contact us: <hello@blazed.space>");
+console.log("**** (c)2022 Ruff Management Inc. All Rights Reserved. ****");
